576eb140bccc4f840959217584345d74fafd18db
Kaitlin Swanson <kateswanson94@gmail.com>
2021-08-06 00:03:00 -0700

    cleanup and planning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import './App.css';
 
+//generate a grid of 9 or 12 spaces 
+//id of those spaces must identify with the image/card and not the space to see if it'ss been chosen twice
+
+//add game instructions at the top left
+//add a section for the score board - score and best score top right 
+
+//randomly assign image cards to spaces 
+//set it to regenerate after each click. useEffect with the dependancy changing for each click
+//add counter + 1 if prev clicks on card === 0 and reset counter if it has a prev click 
+
+//update scoreboard for "best score" every time counter resets - if new score is higher than prev best score, bestscore = new score
+
+//win function for when score reaches 12 (if only 12 images)
+
+
+
 function App() {
   return (
     <div className="App">Hello World
