4ae6aae76e522a48c608056fb32334f788d3e6b8
Kaitlin Swanson <kateswanson94@gmail.com>
2021-08-10 12:22:51 -0700

    add image array

--- src/components/ImageData.js
+++ src/components/ImageData.js
@@ -1,2 +1,85 @@
-import React from 'react';
+export const imgData = [
 
+    {
+        id: 1,
+        img:
+          "https://tvline.com/wp-content/uploads/2015/05/brooklyn-nine-nine-s2-finale.jpg?resize=512,355",
+        text: "Jake Peralta",
+      },
+
+      {
+        id: 2,
+        img:
+          "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-Brooklyn-99-Joe-Lo-Truglio.jpg",
+        text: "Charles Boyle",
+      },
+
+      {
+        id: 3,
+        img:
+          "https://media1.popsugar-assets.com/files/thumbor/SX4VbxxuUjOFpvvjw4KjBgcUDAc/420x0:1260x840/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2020/01/13/367/n/41541398/c93296a75e1d72629e50c6.44423577_/i/best-rosa-diaz-gifs-from-brooklyn-nine-nine.jpg",
+        text: "Rosa Diaz",
+      },
+      {
+        id: 4,
+        img:
+          "https://media.thetab.com/blogs.dir/90/files/2020/04/screenshot-2020-04-20-at-170615.png",
+        text: "Captain Holt",
+      },
+      {
+        id: 5,
+        img:
+          "https://i.pinimg.com/originals/5d/dc/2b/5ddc2b70920dbe6cd3c0e2e51748ca41.jpg",
+        text: "Kevin",
+      },
+      {
+        id: 6,
+        img:
+          "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2018%2F06%2Fstewartrunning-2-2000.jpg&q=85",
+        text: "Cheddar",
+      },
+      {
+        id: 7,
+        img:
+          "https://static.wikia.nocookie.net/brooklynnine-nine/images/b/bf/Amy_in_uniform.png/revision/latest/scale-to-width-down/864?cb=20190907232530",
+        text: "Amy Santiago",
+      },
+      {
+      id: 8,
+      img:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chelsea-peretti-1549029233.jpg",
+      text: "Gina Linetti",
+      },
+      {
+        id: 9,
+        img:
+          "https://i.insider.com/5afc38c61ae6625f008b45ae?width=1061&format=jpeg",
+        text: "Terry Jeffords",
+       },
+
+       {
+        id: 10,
+        img:
+          "https://img.cinemablend.com/filter:scale/quill/f/c/3/c/5/4/fc3c54251b07587550e12746ecde068bb540aad3.png?mw=600",
+        text: "Michael Hitchcock",
+      },
+
+      {
+        id: 11,
+        img:
+          "https://pbs.twimg.com/media/BtUclldIMAAfvuw.jpg",
+        text: "Norm Scully",
+      },
+
+      {
+        id: 12,
+        img:
+          "https://static.wikia.nocookie.net/brooklynnine-nine/images/b/b7/Gray_Star_Mutual-Adrian_Pimento.png/revision/latest?cb=20190511094319",
+        text: "Adrian Pimento",
+      },
+
+
+
+
+
+]
